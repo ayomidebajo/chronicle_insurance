1a503167da2ac1ac83dfbf549ce2641045cd7479
Ayomide Bajo <oluwashinabajo@gmail.com>
2023-12-08 10:20:43 +0100

    add logic for insurance

--- contracts/src/insurance/lib.rs
+++ contracts/src/insurance/lib.rs
@@ -2,139 +2,115 @@
 
 #[ink::contract]
 mod insurance {
+    use ink::storage::Mapping;
 
     /// Defines the storage of your contract.
     /// Add new fields to the below struct in order
     /// to add new static storage fields to your contract.
     #[ink(storage)]
     pub struct Insurance {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        // The contract owner is the contract address
+        owner: AccountId,
+        insurance_premiums: Mapping<AccountId, Balance>,
+        has_insurance: Mapping<AccountId, bool>,
+    }
+
+    const DEFAULT_PREMIUM: Balance = 100;
+
+    // Define events
+    #[ink(event)]
+    pub struct InsurancePurchased {
+        #[ink(topic)]
+        user: AccountId,
+        premium: Balance,
+    }
+
+    #[ink(event)]
+    pub struct ClaimFiled {
+        #[ink(topic)]
+        user: AccountId,
+        amount: Balance,
+    }
+
+    // Define errors
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        NotContractOwner,
+        AlreadyHasInsurance,
+        NoInsurance,
+        NoPremiumProvided,
+        BalanceTooLow,
     }
 
     impl Insurance {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new() -> Self {
+            Self {
+                // Set the contract owner to the contract address
+                owner: Self::env().account_id(),
+                insurance_premiums: Mapping::new(),
+                has_insurance: Mapping::new(),
+            }
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn is_premium(&self, user: AccountId) -> bool {
+            self.insurance_premiums.get(&user).is_some()
         }
 
-        /// Simply returns the current value of our `bool`.
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
-        }
-    }
-
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let insurance = Insurance::default();
-            assert_eq!(insurance.get(), false);
+        pub fn has_insurance(&self, user: AccountId) -> bool {
+            self.has_insurance.get(&user).is_some()
         }
 
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut insurance = Insurance::new(false);
-            assert_eq!(insurance.get(), false);
-            insurance.flip();
-            assert_eq!(insurance.get(), true);
-        }
-    }
+        #[ink(message, payable)]
+        pub fn purchase_insurance(&mut self, premium: Balance) -> Result<(), Error> {
+            let caller = self.env().caller();
 
+            if caller != self.owner {
+                return Err(Error::NotContractOwner);
+            }
 
-    /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
-    ///
-    /// When running these you need to make sure that you:
-    /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
-    /// - Are running a Substrate node which contains `pallet-contracts` in the background
-    #[cfg(all(test, feature = "e2e-tests"))]
-    mod e2e_tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
+            if self.has_insurance(caller.clone()) {
+                return Err(Error::AlreadyHasInsurance);
+            }
 
-        /// A helper function used for calling contract messages.
-        use ink_e2e::build_message;
+            if premium == 0 {
+                return Err(Error::NoPremiumProvided);
+            }
 
-        /// The End-to-End test `Result` type.
-        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+            self.insurance_premiums.insert(caller, &DEFAULT_PREMIUM);
+            self.has_insurance.insert(caller, &true);
 
-        /// We test that we can upload and instantiate the contract using its default constructor.
-        #[ink_e2e::test]
-        async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = InsuranceRef::default();
-
-            // When
-            let contract_account_id = client
-                .instantiate("insurance", &ink_e2e::alice(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            // Then
-            let get = build_message::<InsuranceRef>(contract_account_id.clone())
-                .call(|insurance| insurance.get());
-            let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
+            self.env().emit_event(InsurancePurchased {
+                user: caller,
+                premium,
+            });
 
             Ok(())
         }
 
-        /// We test that we can read and write a value from the on-chain contract contract.
-        #[ink_e2e::test]
-        async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = InsuranceRef::new(false);
-            let contract_account_id = client
-                .instantiate("insurance", &ink_e2e::bob(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
+        #[ink(message)]
+        pub fn file_claim(&mut self) -> Result<(), Error> {
+            let caller = self.env().caller();
 
-            let get = build_message::<InsuranceRef>(contract_account_id.clone())
-                .call(|insurance| insurance.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
+            if self.has_insurance(caller.clone()) == false {
+                return Err(Error::NoInsurance);
+            }
 
-            // When
-            let flip = build_message::<InsuranceRef>(contract_account_id.clone())
-                .call(|insurance| insurance.flip());
-            let _flip_result = client
-                .call(&ink_e2e::bob(), flip, 0, None)
-                .await
-                .expect("flip failed");
+            // Simplified logic: Pay out the insurance premium to the user
+            let amount = self.insurance_premiums.get(&caller).unwrap_or(0_u128);
 
-            // Then
-            let get = build_message::<InsuranceRef>(contract_account_id.clone())
-                .call(|insurance| insurance.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), true));
+            self.env().transfer(caller, amount).expect("Transfer failed");
+
+            // Reset user's insurance status
+            self.has_insurance.insert(caller, &false);
+
+            self.env().emit_event(ClaimFiled {
+                user: caller,
+                amount,
+            });
 
             Ok(())
         }
